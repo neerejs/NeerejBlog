f5498060b451504c74866523e84a53f13219bae7
Neerej Selvakumar <neerejselvakumar@Neerejs-MacBook-Pro.local>
2022-05-20 18:28:04 -0400

    updatedAllTags

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,17 +1,20 @@
 import * as contentful from 'contentful';
 import React, { useState, useEffect } from 'react';
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+import _ from 'lodash'
 
-
+import Container from 'react-bootstrap/Container'
 
 // import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
 
 const BlogList = () => {
     const [items, setItems] = useState([]);
+    const [tags, setTags] = useState([]);
+
 
     useEffect(() => {
         let contentfulClient = contentful.createClient({
@@ -22,43 +25,116 @@ const BlogList = () => {
 
         contentfulClient.getEntries({
             content_type: PLAYER_CONTENT_TYPE_ID,
-            order: '-fields.title', 
-            // 'metadata.tags.sys.id[in]': 'html'
+            order: '-fields.title',
+
         })
             .then(function (entries) {
                 setItems(entries.items);
-                console.log(entries.items)
-                // setLoading(false);
+                setTags(getTags(entries.items))
+
             })
 
     }, []);
 
+    const getTaggedContents = (e, tag) => {
+        e.preventDefault();
+
+        if (tag === 'All') {
+
+        }
+        let contentfulClient = contentful.createClient({
+            accessToken: '1EuIOgC3v2LcxuD2ambb2454ijXnjHKsheuWnPFjGPs',
+            space: 'nvm4509pk8bp'
+        });
+        let PLAYER_CONTENT_TYPE_ID = 'blogs';
+
+        if (tag === 'All') {
+            contentfulClient.getEntries({
+                content_type: PLAYER_CONTENT_TYPE_ID,
+                order: '-fields.title',
+
+            })
+                .then(function (entries) {
+                    setItems(entries.items);
+                })
+        }
+        else {
+            contentfulClient.getEntries({
+                content_type: PLAYER_CONTENT_TYPE_ID,
+                order: '-fields.title',
+                'metadata.tags.sys.id[in]': tag
+            })
+                .then(function (entries) {
+                    setItems(entries.items);
+                })
+        }
+
+    }
+
     const getContents = () => {
         const contentsArray = []
         items.forEach((item, index) => {
-            console.log(item.fields.title);
-            contentsArray.push(<div>
-                <Row>
+            const tagArray = []
+            item.metadata.tags.forEach((t, i) => {
+                tagArray.push(<a href="#" style={{ marginRight: "10px" }}>{t.sys.id}</a>)
+            })
+            contentsArray.push(<div key={index}>
+                <Row className="mt-5">
                     <Col>
-                        <p>{item.fields.title}</p>
+                        <h3>{item.fields.title}</h3>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <p>{documentToReactComponents(item.fields.body)}</p>
+                        <p>{item.fields.shortDescription}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div>{tagArray}</div>
                     </Col>
                 </Row>
             </div>
-
             )
+
         })
+
         return contentsArray;
     }
 
+    const getTags = (items) => {
+        const allItemsTagsArray = []
+        items.forEach((item, index) => {
+            const itemsTagsArray = []
+            item.metadata.tags.forEach((tag, i) => {
+                itemsTagsArray.push(tag.sys.id)
+            })
+            allItemsTagsArray.push('All')
+            allItemsTagsArray.push(...itemsTagsArray)
+        })
+        return _.uniq(allItemsTagsArray)
+
+
+    }
+
+
+    const renderTags = () => {
+        const tagsArray = []
+        tags.forEach((tag, index) => {
+            tagsArray.push(<Button type="button" onClick={(e) => getTaggedContents(e, tag)} style={{ marginRight: "10px" }} href="">{tag}</Button>)
+
+        })
+        return tagsArray
+    }
 
     return (
         <>
-            <Container>
+            <Container >
+                <Row className="mt-5 mb-5">
+                    <Col>
+                        {renderTags()}
+                    </Col>
+                </Row>
                 {getContents()}
             </Container>
 
